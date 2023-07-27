a593e014e0c68013ed34b9bc224529a1304e3872
Pankaj Kumar <88307244+sahu111@users.noreply.github.com>
2023-07-27 05:59:21 +0000

    comparison operators

---
+++ 01_basics/06_comparison.js
@@ -0,0 +1,30 @@
+//NOTE : Comparison ka answer true/false hi aega (agr same datatype ko compare kre)
+// Datatype should be same (iska dhyan rkhna hai)
+
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+// == and >=,<=,>,< works differently
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === 
+
+console.log("2" === 2);
+
+//null aur undefined type ke comparison avoid krna chahiye
